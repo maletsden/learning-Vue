9891561862097a20430f238747355cf52c5f439a
Denys Maletskiy <maletsden@gmail.com>
2017-07-25 14:27:05 +0300

    12-th lesson

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ var i = 1;
 new Vue({
   el: '#vue-app',
   data: {
-    error: false,
-    success: false
+    characters: ['Denys', "john", 'Markiyan'],
+    girls: [
+      {name: 'Diana', age : 16},
+      {name: 'Vite', age : 18},
+      {name: 'Sofia', age: 15},
+      {name: 'Katiya', age: 16}
+    ]
   },
   computed: {
 
