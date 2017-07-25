4b54b4ac3486c94f6d7a03730e8f1ce57d8fe4ca
Denys Maletskiy <maletsden@gmail.com>
2017-07-25 12:21:34 +0300

    10-lesson

--- app.js
+++ app.js
@@ -2,18 +2,15 @@ var i = 1;
 new Vue({
   el: '#vue-app',
   data: {
-    age: 16,
-    a: 0,
-    b: 0
+    red: true,
+    blue: false
   },
   computed: {
-    addToA : function () {
-      console.log('addToA');
-      return this.a + this.age;
-    },
-    addToB : function () {
-      console.log('addToB');
-      return this.b + this.age;
+    compClasses: function () {
+      return {
+        red: this.red,
+        blue: this.blue
+      }
     }
   }
 });
